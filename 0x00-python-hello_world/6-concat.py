7c99738b75e6abf86e629709e21070872ee3d6cb
Adewole-Gege <michaelgegs@gmail.com>
2024-12-23 13:29:22 +0100

    files for tasks 6-13

---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str1 = "Holberton"
+str2 = "School"
+str1 = str1 + " " + str2  # Concatenate the two strings with a space in between
+print(f"Welcome to {str1}!")  # Print the full message
---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+import this  # This module prints "The Zen of Python" when imported
